01d492ec229139a76853077c69b9532deaf0dbae
stringcode <michael@stringcode.co.uk>
2015-11-27 15:06:10 +0000

    trying to fix pod

--- Podyfied/SnowGlobe/Example/Pods/Target Support Files/Pods-SnowGlobe_Example/Pods-SnowGlobe_Example-frameworks.sh
+++ Podyfied/SnowGlobe/Example/Pods/Target Support Files/Pods-SnowGlobe_Example/Pods-SnowGlobe_Example-frameworks.sh
@@ -82,3 +82,10 @@ strip_invalid_archs() {
   fi
 }
 
+
+if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "Pods-SnowGlobe_Example/SnowGlobe.framework"
+fi
+if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "Pods-SnowGlobe_Example/SnowGlobe.framework"
+fi
---
+++ Podyfied/SnowGlobe/Example/Pods/Target Support Files/SnowGlobe/SnowGlobe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SnowGlobe : NSObject
+@end
+@implementation PodsDummy_SnowGlobe
+@end
